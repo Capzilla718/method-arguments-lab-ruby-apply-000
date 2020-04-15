4dee55bd233cc346c62f249be1e6ea1e198add53
Christian A Mesquita <pslimonyc@gmail.com>
2020-04-15 15:19:30 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,5 @@
+failures:
+
+  1) #introduction takes in an argument of a name and puts out a phrase with that name using string interpolation 
+    Failure/error:
+    expect{introduction("Josh")}.
